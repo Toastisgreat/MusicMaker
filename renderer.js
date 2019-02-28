0e01d44817f84f1d26d00c938beb585f5fffda1e
Bobby Murray <bobby@Bobbys-MacBook-Pro.local>
2019-02-27 19:14:53 -0500

    commit

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const { dialog } = require('electron').remote
 const ipc = require('electron').ipcRenderer
 const Vex = require('vexflow')
 const Mousetrap = require('mousetrap');
-
+VF = Vex.Flow
 //Global Variables
 var keylist = {'z': 'a/3', 'x': 'b/3', 'c': 'c/4', 'v': 'd/4', 'b': 'e/4', 'n': 'f/4', 'm': 'g/4', 'a': 'a/4', 's': 'b/4', 'd': 'c/5', 'f': 'd/5', 'g': 'e/5', 'h': 'f/5', 'j': 'g/5', 'q': 'a/5', 'w': 'b/5', 'e': 'c/6', "r": 'd/6', 't': 'e/6', 'y': 'f/6', 'u': 'g/6'}
 var VF = Vex.Flow;
@@ -20,8 +20,6 @@ var y_pos = 40
 var length = 200
 var timeValues = {'w' : 4, 'h': 2, 'q': 1, '8': .5, '16': .25, '32': .125}
 var beat = 0
-// Use the renderer to give the dimensions to the SVG
-
 // Expose the context of the renderer
 var context = renderer.getContext();
 // And give some style to our SVG
@@ -49,19 +47,14 @@ function newnote (e) {
         chord.push(note)
         flat = false
         sharp = false
-        console.log(chord)
     }
     //if the key is enter, then the chord is put no the staff and chord is cleared
     else if (code === 13) {
-    
         //clears the current staff of notes to rewrite on
-        
-        //adds chord to note array
-        if (sharp !== true && flat !== true) { notes.push(new VF.StaveNote({clef: "treble", keys: chord, duration: times[duration]}))}
-        else if (sharp === true && flat !== true) {notes.push(new VF.StaveNote({clef: "treble", keys: chord, duration: times[duration]}).addAccidental(0, new VF.Accidental('#')))}
-        else if (sharp !== true && flat === true) {notes.push(new VF.StaveNote({clef: "treble", keys: chord, duration: times[duration]}).addAccidental(0, new VF.Accidental('b')))}
+        //goes through each chord, and assigns accidentals to keys accordingly
+        notes.push(new VF.StaveNote({clef: "treble", keys:chord, duration: times[duration]}))
         //puts note array on the staff, drawing the notes
-        VF.Formatter.FormatAndDraw(context, stave, notes);
+        VF.Formatter.FormatAndDraw(context, stave, notes)
         //resets variables 
         chord = []
         flat = false
@@ -70,8 +63,9 @@ function newnote (e) {
         beat += timeValues[times[duration]]
         //if the measure is full, add a new measure to the end
         if (beat >= 4) {
+            voice = new VF.Voice({num_beats: 4,  beat_value: 4}); 
             //if reached the end of the line, go to the next tile
-            if (x_pos >= 810) {
+            if (x_pos >= 1210) {
                 //TODO: fix bug where wont use last stave
                 x_pos = 10
                 y_pos += 80
@@ -94,12 +88,12 @@ function newnote (e) {
         else if (duration === 5) {duration = 0}
     }
     // if the key is o, switch flat value
-    else if (code === 79) { 
+    else if (code === 105) { 
         if (flat == true) { flat = false }
         else if (flat === false) { flat = true}
     }
     //if the key is i, switch sharp value
-    else if (code === 73) {
+    else if (code === 111) {
         if (sharp === true) { sharp = false}
         else if (sharp === false) { sharp = true}
     }
